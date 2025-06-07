aed840f25f04b97a794b7aaa0a4acfdf3ea9414f
Felipe Ribeiro <feliper.dev@gmail.com>
2025-06-07 12:15:25 -0300

    Identificação de contexto; Header da equipe; Geração de arquivos .TAB e .LEX

--- main.cpp
+++ main.cpp
@@ -1,13 +1,136 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include "lexer.cpp"
+#include <stack>
+#include <algorithm>
+#include "symbolTable.cpp"
+
+class TypeContext
+{
+public:
+    enum class Context
+    {
+        GLOBAL,
+        DECLARATIONS,
+        FUNCTIONS,
+        FUNCTION_PARAMS,
+        VARIABLE_DECL,
+        ARRAY_DECL,
+        FUNCTION_DECL
+    };
+
+    TypeContext()
+    {
+        contextStack_.push(Context::GLOBAL);
+    }
+
+    void pushContext(Context ctx)
+    {
+        contextStack_.push(ctx);
+    }
+
+    void popContext()
+    {
+        if (contextStack_.size() > 1)
+        {
+            contextStack_.pop();
+        }
+    }
+
+    Context currentContext() const
+    {
+        return contextStack_.top();
+    }
+
+    static std::string mapTypeToCode(TokenType type, bool isArray = false)
+    {
+        if (isArray)
+        {
+            switch (type)
+            {
+            case TokenType::REAL:
+                return "AF";
+            case TokenType::INTEGER:
+                return "AI";
+            case TokenType::STRING:
+                return "AS";
+            case TokenType::CHARACTER:
+                return "AC";
+            case TokenType::BOOLEAN:
+                return "AB";
+            default:
+                return "";
+            }
+        }
+        else
+        {
+            switch (type)
+            {
+            case TokenType::REAL:
+                return "FP";
+            case TokenType::INTEGER:
+                return "IN";
+            case TokenType::STRING:
+                return "ST";
+            case TokenType::CHARACTER:
+                return "CH";
+            case TokenType::BOOLEAN:
+                return "BL";
+            case TokenType::VOID:
+                return "VD";
+            default:
+                return "";
+            }
+        }
+    }
+
+private:
+    std::stack<Context> contextStack_;
+};
+
+double evaluateMathOperation(double left, double right, TokenType op)
+{
+    switch (op)
+    {
+    case TokenType::PLUS:
+        return left + right;
+    case TokenType::MINUS:
+        return left - right;
+    case TokenType::MUL:
+        return left * right;
+    case TokenType::DIV:
+        if (right == 0)
+        {
+            throw std::runtime_error("Nao pode dividir por zero!");
+        }
+        return left / right;
+    case TokenType::MOD:
+        if (right == 0)
+        {
+            throw std::runtime_error("Nao pode dividir por zero!");
+        }
+        return static_cast<int>(left) % static_cast<int>(right);
+    default:
+        throw std::runtime_error("Operador inválido");
+    }
+}
+
+void _teamHeader(std::ofstream &stream)
+{
+    stream << "Código da Equipe: 2" << std::endl
+           << "Componentes:" << std::endl
+           << "    Davih de Andrade Machado Borges Santos; _@aln.senaicimatec.edu.br" << std::endl
+           << "    Felipe Azevedo Ribeiro; _@aln.senaicimatec.edu.br" << std::endl
+           << "    Gabriel Trindade Santana; _@aln.senaicimatec.edu.br" << std::endl
+           << "    Pedro Quadros de Freitas; _@aln.senaicimatec.edu.br" << std::endl
+           << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        std::cerr << "Uso: ./CangaCompiler teste.251\n";
+        std::cerr << "Use: ./CangaCompiler <file_name>.251\n";
         return 1;
     }
     std::string filename = argv[1];
@@ -23,10 +146,14 @@ int main(int argc, char *argv[])
     std::string source = buffer.str();
 
     Lexer lexer(source);
+    SymbolTable symtab;
+    TypeContext typeContext;
+    std::vector<LexemeRecord> lexemes;
 
     TokenType currentType = TokenType::VOID;
     bool isArray = false;
     std::string lastIdentifier;
+    std::stack<double> valueStack;
 
     while (true)
     {
@@ -34,9 +161,59 @@ int main(int argc, char *argv[])
         if (tok.type == TokenType::END_OF_FILE)
             break;
 
+        switch (tok.type)
+        {
+        case TokenType::PROGRAM:
+            typeContext.pushContext(TypeContext::Context::GLOBAL);
+            break;
+        case TokenType::DECLARATIONS:
+            typeContext.pushContext(TypeContext::Context::DECLARATIONS);
+            break;
+        case TokenType::ENDDECLARATIONS:
+            typeContext.popContext();
+            break;
+        case TokenType::FUNCTIONS:
+            typeContext.pushContext(TypeContext::Context::FUNCTIONS);
+            break;
+        case TokenType::ENDFUNCTIONS:
+            typeContext.popContext();
+            break;
+        case TokenType::VAR:
+            typeContext.pushContext(TypeContext::Context::VARIABLE_DECL);
+            break;
+        case TokenType::FUNCTYPE:
+            typeContext.pushContext(TypeContext::Context::FUNCTION_DECL);
+            break;
+        case TokenType::LPAREN:
+            if (typeContext.currentContext() == TypeContext::Context::FUNCTION_DECL)
+            {
+                typeContext.pushContext(TypeContext::Context::FUNCTION_PARAMS);
+            }
+            break;
+        case TokenType::RPAREN:
+            if (typeContext.currentContext() == TypeContext::Context::FUNCTION_PARAMS)
+            {
+                typeContext.popContext();
+            }
+            break;
+        case TokenType::LBRACK:
+            isArray = true;
+            break;
+        case TokenType::RBRACK:
+            isArray = false;
+            break;
+        case TokenType::SEMI:
+            if (typeContext.currentContext() == TypeContext::Context::VARIABLE_DECL ||
+                typeContext.currentContext() == TypeContext::Context::FUNCTION_DECL)
+            {
+                typeContext.popContext();
+            }
+            break;
+                }
+
         if (tok.type != TokenType::IDENT)
         {
-            //
+            lexemes.push_back({tok.lexeme, tok.type, -1, tok.line});
         }
     }
 
@@ -47,18 +224,51 @@ int main(int argc, char *argv[])
                             : filename.substr(0, posdot));
     std::ofstream lexOut(base + ".LEX");
 
-    lexOut << "LEX report for " << filename << "\n";
-    lexOut << "Lexeme\tAtomCode\tTableIdx\tLine\n";
+    _teamHeader(lexOut);
 
-    // Preenchimento do arquivo .LEX
-
+    for (auto &r : lexemes)
+    {
+        lexOut
+            << "Lexeme: " << r.lexeme << ", Código: "
+            << (SymbolTable::tokenTypeToString(r.type)) << ", ÍndiceTabSimb: "
+            << (r.tableIndex > 0 ? std::to_string(r.tableIndex) : "-") << ", Linha: "
+            << r.line << ".\n";
+    }
     lexOut.close();
 
     std::ofstream tabOut(base + ".TAB");
-    tabOut << "Entry\tAtomCode\tLexeme\tLenBefore\tLenAfter\tType\tLines\n";
 
-    // Preenchimento do arquivo .TAB
+    std::vector<std::pair<std::string, SymbolTable::SymbolInfo>> syms;
+    for (auto &p : symtab.all())
+        syms.push_back(p);
+    std::sort(syms.begin(), syms.end(),
+              [](auto &a, auto &b)
+              { return a.second.entry < b.second.entry; });
 
+    _teamHeader(tabOut);
+
+    for (size_t i = 0; i < syms.size(); ++i)
+    {
+        auto &p = syms[i];
+        auto &info = p.second;
+        tabOut
+            << "Entrada: " << info.entry << ", Codigo: "
+            << info.atomCode << ", Lexeme: " << info.lexeme << ",\n"
+            << "QtdCharAntesTrunc: " << info.lenBefore << ", QtdCharDepoisTrunc: "
+            << info.lenAfter << ",\n"
+            << "TipoSimb: " << info.type << ", Linhas: {";
+        for (size_t j = 0; j < info.lines.size(); ++j)
+        {
+            if (j)
+                tabOut << ", ";
+            tabOut << info.lines[j];
+        }
+        tabOut << "}.\n";
+        if (i < syms.size() - 1)
+        {
+            tabOut << "----------------------------------------------------------------------------------------------------\n";
+        }
+    }
     tabOut.close();
 
     std::cout << "Arquivos gerados: " << base << ".LEX e " << base << ".TAB\n";
